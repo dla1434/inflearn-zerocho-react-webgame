af1e232ea1dae4faa3f76772769d12814c05069f
lim jong hyun <dla1434@naver.com>
2021-05-29 19:00:58 +0900

    this.state, this.props를 구조문법으로 생략하여 사용하기

--- JH_3.숫자야구/NumberBaseball.jsx
+++ JH_3.숫자야구/NumberBaseball.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Try from './Try';
 
 //숫자 네 개를 랜덤하게 뽑는 함수
+/*
 function getNumbers() {
   const candidate = [1, 2, 3, 4, 5, 5, 6, 7, 8, 9];
   const array = [];
@@ -12,25 +13,41 @@ function getNumbers() {
   console.log('getNumbers', array);
   return array;
 }
+ */
 
 class NumberBaseball extends Component {
+  getNumbers = () => {
+    const candidate = [1, 2, 3, 4, 5, 5, 6, 7, 8, 9];
+    const array = [];
+    for (let i = 0; i < 4; i += 1) {
+      const chosen = candidate.splice(
+        Math.floor(Math.random() * (9 - i)),
+        1
+      )[0];
+      array.push(chosen);
+    }
+    console.log('getNumbers', array);
+    return array;
+  };
+
   state = {
     result: '',
     value: '',
-    answer: getNumbers(), //ex: [1,3,5,7]
+    answer: this.getNumbers(), //ex: [1,3,5,7]
     tries: [], //블변성을 지키지 위해서 push를 사용하면 안 됨
   };
 
   //화살표 함수를 써야 한다.
   //안 쓴다면 bind를 별도로 해줘야 하고..그러러면 constructor도 선언해줘야 한다.
   onSubmitForm = (e) => {
+    const { value, tries, answer } = this.state;
     e.preventDefault();
-    if (this.state.value === this.state.answer.join('')) {
+    if (value === answer.join('')) {
       this.setState({
         result: '홈런',
-        // this.state.tries.push() //블변성을 지키지 위해서 push를 사용하면 안 됨
+        // tries.push() //블변성을 지키지 위해서 push를 사용하면 안 됨
         //배열 안에 객체 넣기
-        tries: [...this.state.tries, { try: this.state.value, result: '홈런' }],
+        tries: [...tries, { try: value, result: '홈런' }],
       });
       alert('게임을 다시 시작합니다.');
       this.setState({
@@ -40,10 +57,10 @@ class NumberBaseball extends Component {
       });
     } else {
       //답인 틀린 경우 처리
-      const answerArray = this.state.value.split('').map((v) => parseInt(v));
+      const answerArray = value.split('').map((v) => parseInt(v));
       let strike = 0;
       let ball = 0;
-      if (this.state.tries.length >= 9) {
+      if (tries.length >= 9) {
         this.setState({
           result: `10번 넘게 틀려서 실패! 답은 ${answer.join(',')}였습니다!`,
         });
@@ -55,16 +72,16 @@ class NumberBaseball extends Component {
         });
       } else {
         for (let i = 0; i < 4; i += 1) {
-          if (answerArray[i] === this.state.answer[i]) {
+          if (answerArray[i] === answer[i]) {
             strike += 1;
-          } else if (this.state.answer.includes(answerArray[i])) {
+          } else if (answer.includes(answerArray[i])) {
             ball += 1;
           }
           this.setState({
             tries: [
-              ...this.state.tries,
+              ...tries,
               {
-                try: this.state.value,
+                try: value,
                 result: `${strike} 스트라이크, ${ball} 볼입니다.`,
               },
             ],
