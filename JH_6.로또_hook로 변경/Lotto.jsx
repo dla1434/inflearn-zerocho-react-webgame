b1a77bb7dd16a91c27195742b2f1ebfbeefaf45d
lim jong hyun <dla1434@naver.com>
2021-06-02 00:29:22 +0900

    로또 - 클래스 컴포넌트 완성

--- JH_6.로또_hook로 변경/Lotto.jsx
+++ JH_6.로또_hook로 변경/Lotto.jsx
@@ -1,12 +1,22 @@
-import React, { useState, useRef, useEffect, useMemo, useCallback } from 'react';
+import React, {
+  useState,
+  useRef,
+  useEffect,
+  useMemo,
+  useCallback,
+} from 'react';
 import Ball from './Ball';
 
 function getWinNumbers() {
   console.log('getWinNumbers');
-  const candidate = Array(45).fill().map((v, i) => i + 1);
+  const candidate = Array(45)
+    .fill()
+    .map((v, i) => i + 1);
   const shuffle = [];
   while (candidate.length > 0) {
-    shuffle.push(candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]);
+    shuffle.push(
+      candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]
+    );
   }
   const bonusNumber = shuffle[shuffle.length - 1];
   const winNumbers = shuffle.slice(0, 6).sort((p, c) => p - c);
@@ -37,7 +47,8 @@ const Lotto = () => {
         clearTimeout(v);
       });
     };
-  }, [timeouts.current]); // 빈 배열이면 componentDidMount와 동일
+  }, [timeouts.current]);
+  // 빈 배열이면 componentDidMount와 동일
   // 배열에 요소가 있으면 componentDidMount랑 componentDidUpdate 둘 다 수행
 
   useEffect(() => {
@@ -58,7 +69,9 @@ const Lotto = () => {
     <>
       <div>당첨 숫자</div>
       <div id="결과창">
-        {winBalls.map((v) => <Ball key={v} number={v} />)}
+        {winBalls.map((v) => (
+          <Ball key={v} number={v} />
+        ))}
       </div>
       <div>보너스!</div>
       {bonus && <Ball number={bonus} onClick={onClickRedo} />}
